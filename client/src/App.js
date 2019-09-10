ff0911253d4a7fb4a04b4be65108b5f63d34a9b1
ssmith <shawn.t.smith.777@gmail.com>
2019-09-09 22:15:36 -0500

    added Apollo client code to public/react side of APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
+import logo from './spacex-logo.png';
+import Launches from './component/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className='App'>
-      <h1>spacex</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='Spacex'
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
-
 const app = express();
+const cors = require('cors');
+// Allow cross-origin
+app.use(cors());
 
 app.use(
   '/graphql',
