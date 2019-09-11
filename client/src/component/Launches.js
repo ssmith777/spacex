d41c5f6b3a4e3de8e4b1c05f354b5cd46ac398b7
ssmith <shawn.t.smith.777@gmail.com>
2019-09-11 01:01:27 -0500

    corrected index issues in Launchitems

--- client/src/component/Launches.js
+++ client/src/component/Launches.js
@@ -23,28 +23,8 @@ export default function Launches() {
   return (
     <Fragment>
       {data.launches.map((launch, index) => (
-        <Launchitems key={launch.flightnumber} launch={launch} key={index} />
+        <Launchitems launch={launch} key={index} />
       ))}
     </Fragment>
   );
 }
-
-// export class Launches extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1 className='display-4 my-3'>Launches</h1>
-//         <Query query={LAUNCHES_QUERY}>
-//           {({ loading, error, data }) => {
-//             if (loading) return <h4>Loading...</h4>;
-//             if (error) console.log(error);
-//             console.log(data);
-//             return <h1>test</h1>;
-//           }}
-//         </Query>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Launches;
